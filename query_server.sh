db56864ca982c27fe16936cc97806555380e82d2
draytonh <draytonh@trgn.usc.edu>
2019-09-17 13:24:45 -0700

    added help.

--- query_server.sh
+++ query_server.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/bash
-echo "Current Server: $HOSTNAME!"
+if [ "$1" == "--h" ]; then
+    echo 'Usage: query_server.sh [OPTION]'
+    echo 'Prints the name of the current server as: "Current Server:[servername]'
+    echo '-l  Prints the username with the servername'
+    echo 'Exit status:'
+    echo '0  if OK,'
+    echo '1  Problem with command line';
+elif [ "$1" == "-l" ]; then
+     echo "Current Server:$USER@$HOSTNAME!";
+else
+    exit 0
+fi
